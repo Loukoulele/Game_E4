60ff53e53f2c4a2c8d8c3c0115f53ea208161302
Julien <julien1105@outlook.fr>
2018-04-27 17:35:09 +0200

    v0.7 "header & sidemenu 2"

--- game/header/header.php
+++ game/header/header.php
@@ -12,7 +12,7 @@ if(!isset($_SESSION['admin']))
 <style><?php include 'header.css'; ?></style>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#"><img src="../img/game.png" class="sizelogo"></a>
       <form class="" action="../my_functions/my_class.php" method="post">
       <button class="btn bg-dark" name="action"><i class="fa fa-power-off " aria-hidden="true"></i></button>
     </form>
