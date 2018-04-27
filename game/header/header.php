3c023339948b4767ad5538d7c905e81d348595e5
Julien <julien1105@outlook.fr>
2018-04-27 17:26:46 +0200

    v0.7 "header & sidemenu 1"

--- game/header/header.php
+++ game/header/header.php
@@ -9,27 +9,12 @@ if(!isset($_SESSION['admin']))
 }
 
  ?>
+<style><?php include 'header.css'; ?></style>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
   <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-    <form action="../my_functions/my_class.php" method="post">
-      <button class="btn btn-primary logout" name="action"><i class="fa fa-power-off" aria-hidden="true"></i></button>
+      <form class="" action="../my_functions/my_class.php" method="post">
+      <button class="btn bg-dark" name="action"><i class="fa fa-power-off " aria-hidden="true"></i></button>
     </form>
   </div>
 </nav>
